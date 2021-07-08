fee07e6634340c2f8d5dc4aa74eb6d903299730f
Jester Lopez <Jesmark1999@gmail.com>
2021-07-08 13:45:16 -0600

    Init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Coming from "./source/ComingSoon.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={Coming} className="App-logo" alt="logo" />
+        {/* <p>
+          Edit <code> src / App.js </code> and save to reload.{" "}
         </p>
         <a
           className="App-link"
@@ -15,9 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Learn React{" "}
+        </a>{" "} */}
+      </header>{" "}
     </div>
   );
 }
